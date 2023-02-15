427448e2a97ecd20bf0d0d5fc221904490dca82d
Carl Pagels <carl.pagels@fortnox.se>
2023-02-15 08:42:35 +0100

    Align text to center

--- ui/src/components/aboutandcontact/About.tsx
+++ ui/src/components/aboutandcontact/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Heading, Stack, Text } from "@chakra-ui/react";
+import { AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Center, Heading, Stack, Text } from "@chakra-ui/react";
 
 export const About: React.FC = () => {
     return (
@@ -14,13 +14,16 @@ export const About: React.FC = () => {
             </h2>
             <AccordionPanel pb={4}>
                 <Stack rowGap={5}>
-                <Heading as="h4" size="md">
-                    Welcome to TibiaPal.com everyone! :)
-                </Heading>
-                <Text>
+                    <Center>
+                        <Heading as="h4" size="md">
+                            Welcome to TibiaPal.com everyone! :)
+                        </Heading>
+                    </Center>
+
+                <Text align={"center"}>
                     TibiaPal was originally started as 'TibiaLootSplit.com', which most of you know and love.
                 </Text>
-                <Text>
+                <Text align={"center"}>
                     I created TibiaLootSplit because I was tired of always manually splitting the teamhunt loot - and so
                     TibiaLootSplit.com was born. TibiaLootSplit's popularity spread like wildfire through the community
                     thanks
@@ -29,7 +32,7 @@ export const About: React.FC = () => {
                     think of I already implemented.
                     That's when I decided to branch out and built TibiaPal.com!
                 </Text>
-                <Text>
+                <Text align={"center"}>
                     TibiaPal.com is an
                     extension of TibiaLootSplit and has many more tools for the community to use to enhance their Tibia
                     experience! I continue working on a number of tools and requests (keep them coming!), so please stay
@@ -37,7 +40,7 @@ export const About: React.FC = () => {
                     for future Tibia tool updates :)
                     Much love and see you in Tibia!
                 </Text>
-                <Text as="b">
+                <Text as="b" align={"center"}>
                     Yours,
                     Kusnier
                 </Text>
